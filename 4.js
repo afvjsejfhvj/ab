0f59220b1ec3dd1cc84519e68cc9296aaea1572a
mateus <150347661+afvjsejfhvj@users.noreply.github.com>
2024-07-31 09:08:11 -0300

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,10 @@
+let idade = 1; 
+
+if(idade >= 18){
+if ( idade >= 18 && idade <=70){
+    console.log('voto obrigatorio')
+}else{
+    console.log('voto opsional')
+}}else{
+    console.log('nao pode votar')
+}
